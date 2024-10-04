afd1c2967ad04344db8b4a26d7350514dd718667
flzl2008 <goddlaek@naver.com>
2024-10-04 23:31:07 +0900

    [미션2] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/hyeonggeun/accountbook/AccountbookApplication.kt
@@ -0,0 +1,11 @@
+package com.hyeonggeun.accountbook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccountbookApplication
+
+fun main(args: Array<String>) {
+	runApplication<AccountbookApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyeonggeun/accountbook/AccountbookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyeonggeun.accountbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountbookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
